0427bf7a2ae6024daed11c58b7af3680c55ee4ec
notusknot <>
2025-07-16 10:57:38 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Monkey";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        go
+        gopls
+      ];
+    };
+  };
+}
